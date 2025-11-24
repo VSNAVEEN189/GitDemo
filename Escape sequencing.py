dc1609fd6e59fcbe4d0681ea4afc00ab558f81db
V s naveen kumar ambati <vsnaveenkumarambati2004@gmail.com>
2025-11-24 16:51:09 +0530

    initial commit

---
+++ Escape sequencing.py
@@ -0,0 +1,19 @@
+#Escape sequences are special characters that are used for some special insider.
+
+#\n- new/next line
+#\t- tab
+#\\- backslash
+#\'- inserts a single quote inside a single-quoted string
+
+#\n- Gives new line or next line
+print("Hello everyone. \nHow are you?")
+
+#\t- To give some spaces in string
+print("John\t20")
+
+#\\- to insert single backslash
+print("new\\old")
+
+#\'- To insert single quote in single quoted string as well with double quoted string
+print('This is Python\'s class')
+
---
+++ String/Fstring.py
@@ -0,0 +1,8 @@
+name = "John"
+sub1 = 78
+sub2 = 87
+sub3 = 83
+
+#print(f"{name} scored {sub1 + sub2 + sub3} marks in total")
+percent = int((sub1 + sub2 + sub3) / 3)
+print(f"{name} got {percent}%")
